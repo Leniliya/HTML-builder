f996f0c3bd7311a5c61e870fefe8c10d93205a30
Leniliya <antonova.aliona2010@yandex.ru>
2023-05-09 02:37:51 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const filesPath = path.join(__dirname, 'files');
+const copyFilesPath = path.join(__dirname, 'files-copy');
 
+fs.rm(copyFilesPath, { recursive: true, force: true }, () => {
+  fs.promises.mkdir(copyFilesPath, { recursive: true })
+    .then(
+      fs.promises.readdir(filesPath)
+        .then(arr => {
+          arr.forEach(item => {
+            const itemPath = path.join(filesPath, item);
+            const copyItemPath = path.join(copyFilesPath, item);
+            fs.promises.copyFile(itemPath, copyItemPath);
+            console.log(copyItemPath);
+          });
+        }));
+});
